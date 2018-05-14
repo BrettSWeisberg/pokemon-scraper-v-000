076f0f833fe0442290b9be89fe84e88a085bb927
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-14 21:58:24 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,6 +15,18 @@ class Pokemon
     @@all
   end
 
+  def self.create_table
+    sql =  << -sql
+    CREATE TABLE IF NOT EXISTS pokemon(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    type TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+
   def self.save(name, type, db, database_connection)
     database_connection.execute("INSERT INTO pokemon(name,type,db) VALUES (?,?,?)",name,type,db)
   end
