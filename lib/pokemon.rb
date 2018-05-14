322529a47b7b3af775cdd85a7cac8e71a3d2ff3c
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-14 22:36:28 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -32,7 +32,7 @@ class Pokemon
     VALUES (?,?,?)
     SQL
 
-    DB[:conn].execute(sql,self.name,self.type, self.db)
+    execute(sql,self.name,self.type, self.db)
 
   #  @id = DB[:conn].execute("SELECT last_inserted() FROM pokemon")[0][0]
   end
